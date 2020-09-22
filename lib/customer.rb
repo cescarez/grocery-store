7f99ebf2cc2f0e7edb454d3ed25e56f51341cee4
Christabel Sebastian <christabel.escarez@gmail.com>
2020-09-22 16:29:39 -0700

    Wave 1 implemented to basic specifications, passes tests

--- lib/customer.rb
+++ lib/customer.rb
@@ -5,24 +5,12 @@
 
 class Customer
   attr_reader :id
-  attr_accessor :email, :delivery_address
-
-  def initialize(id_num)
-    @id = id_num
-    @email = :unknown
-    @delivery_address = { street: :unknown, city: :unknown, state: :unknown, country: :unknown }
-  end
-
-  def initialize(id_num, email)
-    @id = id_num
-    @email = email
-    @delivery_address = { street: :unknown, city: :unknown, state: :unknown, country: :unknown }
-  end
+  attr_accessor :email, :address
 
   def initialize(id_num, email, delivery_address_hash)
     @id = id_num
     @email = email
-    @delivery_address = delivery_address_hash
+    @address = delivery_address_hash
   end
 
 
