f73e1a60e5e0735d903fe6349f2d541584374c67
Christabel Sebastian <christabel.escarez@gmail.com>
2020-09-22 20:46:42 -0700

    Removed TODO

--- test/customer_test.rb
+++ test/customer_test.rb
@@ -32,7 +32,6 @@ describe "Customer Wave 1" do
   end
 end
 
-# TODO: remove the 'x' in front of this block when you start wave 2
 describe "Customer Wave 2" do
   describe "Customer.all" do
     it "Returns an array of all customers" do
