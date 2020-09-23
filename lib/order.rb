ac369f6963bf3f9ca8dc2b6d9cc13a99fb0951ea
Christabel Sebastian <christabel.escarez@gmail.com>
2020-09-22 17:08:36 -0700

    Wave 1 implemented to basic specifications, passes tests

--- lib/order.rb
+++ lib/order.rb
@@ -2,34 +2,37 @@
 # Last updated, Sept 22, 2020
 # Ada C14
 # Grocery Store: Order Class
-
 class Order
   FULFILLMENTS = [:pending, :paid, :processing, :shipped, :complete]
-  TAX_RATE = .075
+  TAX_RATE = 0.075
 
   attr_reader :id, :products, :fulfillment_status
   attr_accessor :customer
 
-  def initialize(id_num, customer_name)
+  def initialize(id_num, products, customer_object,fulfillment_status = FULFILLMENTS[0])
     @id = id_num
-    @customer = customer_name
-    @products = {}
-    @fulfillment_status = FULFILLMENTS[0]
+    @customer = customer_object
+    @products = (products && !products.empty?) ? products : Hash.new(0)
+    @fulfillment_status = check_fulfillment_status(fulfillment_status)
   end
 
   def total
-    sub_total = @products.sum {|product, price| price}
+    sub_total = @products.sum {|product_name, price| price}
     return ((1 + TAX_RATE) * sub_total).round(2)
   end
 
   def add_product(product_name, price)
-    raise ArgumentError, "Invalid price." if (num.to_f == 0 && !num.match(/[0]+[.]?[0]*/))
+    product_name = product_name.downcase
+
+    raise ArgumentError, "Invalid price." if (price.to_f == 0 && !price.match(/[d]+[.]?[d]*/))
     raise ArgumentError, "Product was already in Order #{@id} and cannot be added." if @products.include?(product_name)
 
-    @products << { product_name => price.to_f.round(2) }
+    @products[product_name] = price.to_f.round(2)
   end
 
   def remove_product(product_name)
+    product_name = product_name.downcase
+
     raise ArgumentError, "Product, #{product_name}, is not in Order #{@id} and cannot be deleted." if !@products.include?(product_name)
 
     @products.delete(product_name)
@@ -53,7 +56,7 @@ class Order
 
   def check_fulfillment_status(current_status)
     raise ArgumentError, "Invalid order status." if !FULFILLMENTS.include?(current_status)
-    return @fulfillment_status
+    return current_status
   end
 
 end
