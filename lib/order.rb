74a4dfaab4e5a3f29c5a012176242a99425ecf8f
Christabel Sebastian <christabel.escarez@gmail.com>
2020-09-23 16:57:43 -0700

    Added .to_i to passed in parameter during .filter of Order.find_by_customer

--- lib/order.rb
+++ lib/order.rb
@@ -63,7 +63,7 @@ class Order
   end
 
   def self.find_by_customer(customer_id)
-    return all.filter { |order| order.customer.id == customer_id }
+    return all.filter { |order| order.customer.id == customer_id.to_i }
   end
 
   private
