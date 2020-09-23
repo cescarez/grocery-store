3dccc10b23ea3a50648468d4f4c3e3718e49a20c
Christabel Sebastian <christabel.escarez@gmail.com>
2020-09-23 07:48:43 -0700

    Moved #get_addresses and #flatten_address to `private` and refactored #flatten_address

--- lib/customer.rb
+++ lib/customer.rb
@@ -15,13 +15,6 @@ class Customer
     @address = delivery_address_hash
   end
 
-  def self.get_addresses(two_d_array)
-    addresses = two_d_array.map do |customer|
-      { street: customer[2], city: customer[3], state: customer[4], zip: customer[5] }
-    end
-    return addresses
-  end
-
   def self.all
     external_customer_file = CSV.read('data/customers.csv').map { |row| row.to_a }
 
@@ -39,16 +32,23 @@ class Customer
     return self.all.find { |customer| customer.id == id }
   end
 
-  def flatten_address(address_hash)
-    return (address_hash[:street] + ',' + address_hash[:city] + ',' + address_hash[:state] + ',' + address_hash[:zip])
-  end
-
   def self.save(filename, new_customer)
     #mode "a" option = append write-only
     CSV.open(filename, "a") do |csv|
       csv << new_customer.id + ',' + new_customer.email + ',' + flatten_address(new_customer.address)
     end
-
   end
 
+  private
+
+  def self.get_addresses(two_d_array)
+    addresses = two_d_array.map do |customer|
+      { street: customer[2], city: customer[3], state: customer[4], zip: customer[5] }
+    end
+    return addresses
+  end
+
+  def flatten_address(address_hash)
+    return address_hash.values.join(',')
+  end
 end
