9f1fa6361ac30832d191dee996ef6b48db1a417b
Christabel Sebastian <christabel.escarez@gmail.com>
2020-09-22 21:34:16 -0700

    Created a helper method (self.get_addresses) to call in self.all

--- lib/customer.rb
+++ lib/customer.rb
@@ -15,13 +15,17 @@ class Customer
     @address = delivery_address_hash
   end
 
-  #TODO: DRY/refactor self.all method
+  def self.get_addresses(two_d_array)
+    addresses = two_d_array.map do |customer|
+      { street: customer[2], city: customer[3], state: customer[4], zip: customer[5] }
+    end
+    return addresses
+  end
+
   def self.all
     external_customer_file = CSV.read('data/customers.csv').map { |row| row.to_a }
 
-    addresses = external_customer_file.map do |customer|
-      { street: customer[2], city: customer[3], state: customer[4], zip: customer[5] }
-  end
+    addresses = self.get_addresses(external_customer_file)
 
     customers = []
     external_customer_file.each_with_index do |customer, i|
