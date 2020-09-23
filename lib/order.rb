47c6a7b70428be0c4d52a8bea5cdbd04d5213156
Christabel Sebastian <christabel.escarez@gmail.com>
2020-09-22 20:43:58 -0700

    Order#all written to basic specification,

--- lib/order.rb
+++ lib/order.rb
@@ -3,17 +3,27 @@
 # Ada C14
 # Grocery Store: Order Class
 class Order
-  FULFILLMENTS = [:pending, :paid, :processing, :shipped, :complete]
+  VALID_STATUSES = [:pending, :paid, :processing, :shipped, :complete]
   TAX_RATE = 0.075
 
   attr_reader :id, :products, :fulfillment_status
   attr_accessor :customer
 
-  def initialize(id_num, products, customer_object,fulfillment_status = FULFILLMENTS[0])
+  def initialize(id_num, products, customer_object,fulfillment_status = VALID_STATUSES[0])
     @id = id_num
     @customer = customer_object
     @products = (products && !products.empty?) ? products : Hash.new(0)
     @fulfillment_status = check_fulfillment_status(fulfillment_status)
+  end
+
+  def check_fulfillment_status(current_status)
+    if current_status.class != Symbol
+      current_status = current_status.downcase.to_sym
+    end
+
+    raise ArgumentError, "Invalid order status." if !VALID_STATUSES.include?(current_status)
+
+    return current_status
   end
 
   def total
@@ -37,26 +47,45 @@ class Order
 
     @products.delete(product_name)
   end
+  #
+  # def pay
+  #   @fulfillment_status = VALID_STATUSES[1]
+  # end
+  #
+  # def process
+  #   @fulfillment_status = VALID_STATUSES[2]
+  # end
+  #
+  # def ship
+  #   @fulfillment_status = VALID_STATUSES[-2]
+  # end
+  #
+  # def deliver
+  #   @fulfillment_status = VALID_STATUSES[-1]
+  # end
 
-  def pay
-    @fulfillment_status = FULFILLMENTS[1]
-  end
+  def self.all
+    external_orders_file = CSV.read('data/orders.csv').map { |row| row.to_a }
+    #id, customer object, product hash, status
 
-  def process
-    @fulfillment_status = FULFILLMENTS[2]
-  end
+    all_products = []
+    external_orders_file.each do |order|
+      this_order_hash = {}
+      order_products = order[1].split(';')
+      order_products.each do |product|
+        each_product = product.split(':')
+        this_order_hash[each_product[0]] = each_product[1].to_f
+      end
+      all_products << this_order_hash
+    end
 
-  def ship
-    @fulfillment_status = FULFILLMENTS[-2]
-  end
 
-  def deliver
-    @fulfillment_status = FULFILLMENTS[-1]
-  end
+    orders = []
+    external_orders_file.each_with_index do |order, i|
+      orders << Order.new(order[0].to_i, all_products[i], Customer.find(order[2].to_i), order[3])
+    end
 
-  def check_fulfillment_status(current_status)
-    raise ArgumentError, "Invalid order status." if !FULFILLMENTS.include?(current_status)
-    return current_status
+    return orders
   end
 
 end
